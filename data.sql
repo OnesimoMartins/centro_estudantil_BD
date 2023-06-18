3add475ceebfc22f7c4448189161e1e0e34a4688
Onesimo Martins <Ogouveia86@gmail.com>
2023-06-18 12:07:58 +0100

    feat: modificações para o requisito 2

--- data.sql
+++ data.sql
@@ -1,9 +1,15 @@
 /*  DADOS  */
 
-insert into classe(numero) values(1,2,4,5,6);
+use centro_estudantil;
+
+insert into periodo_escolar(data_inicio,data_fim) values 
+('2020-01-09','2021-01-09'),('2021-01-09','2022-01-09'),('2022-01-09','2022-01-03'),('2023-01-03','2024-01-09');
+
+insert into classe(numero) values(1),(2),(3),(4),(5),(6);
 
 insert into disciplina(nome) values
 ('Matemática'),('Língua portuguesa'),('Ed.Musical'),('Estudo do meio'),('Ed.Moral'),('Química'),('Física');
 
-insert into classe_disciplina(id_classe,id_disciplina) values 
-(1,1),(2,1),(3,1),(4,1),(5,1);
+insert into classe_disciplina(id_classe,id_disciplina,periodo_escolar_id) values 
+(1,1,1),(2,1,1),(3,1,1),(4,1,1),(5,1,1),
+(1,2,1),(1,3,1),(2,3,1),(4,3,1),(6,7,1),(6,6,1);
--- querys.sql
+++ querys.sql
@@ -1,1 +1,12 @@
 select * from classe_disciplina;
+select * from classe;
+
+-- seleccionar todas disciplinas de uma classe
+select c.numero as classe , d.nome as disciplina ,
+ CONCAT(DATE_FORMAT(p.data_inicio,'%d/%m/%Y'),' a ', DATE_FORMAT(p.data_fim,'%d/%m/%Y') ) as  periodo_escolar
+
+ from classe_disciplina  cd
+inner join disciplina d on d.id= cd.id_disciplina
+inner join classe c on c.id=cd.id_classe
+inner join periodo_escolar p on cd.periodo_escolar_id= p.id
+where p.id=1 and c.numero=1;
