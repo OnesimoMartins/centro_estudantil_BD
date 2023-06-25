997e88e108b8420768764f5a3dcac9ecc59efca8
Onesimo Martins <Ogouveia86@gmail.com>
2023-06-25 19:42:03 +0100

    feat: 3. lista das avaliações [rever-distinct]

--- querys.sql
+++ querys.sql
@@ -30,4 +30,29 @@ where p.id=1 and c.numero=1;
 
 -- 3.	Lista dos alunos por grupo 
 
+select distinct CONCAT(year(pe.data_inicio),'/',RIGHT((year(pe.data_fim)),2) ) as PERIODO_ESCOLAR
+       ,concat(c.numero,'a') as CLASSE
+       ,g.numero as GRUPO
+       ,d.nome as DISCIPLINA
+       ,al.id as CODIGO_ALUNO
+       ,al.nome as NOME
+       ,al.sobrenome as APELIDO
+       ,GET_DISCIPLINA_RESULT(d.id,al.id,pe.id) as NOTA
+
+from avaliacao avaliacao
+
+         inner join periodo_escolar_classe_aluno peca on peca.id_aluno = avaliacao.id_periodo_escolar
+         inner join aluno al on al.id = avaliacao.id_aluno
+         inner join periodo_escolar pe on pe.id = peca.periodo_escolar_id
+         inner join classe c on c.id = peca.id_classe
+
+         inner join aluno_grupo ag on ag.id_aluno=al.id and ag.periodo_escolar_id=pe.id
+         inner join grupo g on g.id=ag.id_grupo
+
+         inner join disciplina d on d.id=avaliacao.id_disciplina
+
+where pe.id=1 and al.id=1;
+
+-- 4.	Lista dos alunos por grupo 
+
 # where 0=0 order by (pe.id);
