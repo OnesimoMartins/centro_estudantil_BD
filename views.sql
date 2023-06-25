58826722c089fb33668dfa9e861e6570e2951c58
Onesimo Martins <Ogouveia86@gmail.com>
2023-06-25 22:36:50 +0100

    feat: view #1 [adicao do numero]

--- views.sql
+++ views.sql
@@ -1,13 +1,18 @@
-create view ALUNOS_POR_GRUPO_VIEW as select CONCAT(year(pe.data_inicio),'/',RIGHT((year(pe.data_fim)),2) ) as PERIODO_ESCOLAR,
+drop view ALUNOS_POR_GRUPO_VIEW;
+create view ALUNOS_POR_GRUPO_VIEW AS 
+SELECT 
+       CONCAT(year(pe.data_inicio),'/',RIGHT((year(pe.data_fim)),2) ) as PERIODO_ESCOLAR,
        concat(c.numero,'a') as CLASSE,
        g.numero as GRUPO,
        a.id as CODIGO_ALUNO,
        a.nome as NOME,
        a.sobrenome as APELIDO
+       ,ROW_NUMBER() OVER (ORDER BY a.id) AS NUMERO 
 from aluno a
          inner join periodo_escolar_classe_aluno peca on peca.id_aluno = a.id
          inner join periodo_escolar pe on pe.id = peca.periodo_escolar_id
          inner join aluno_grupo ag on ag.id_aluno=a.id and ag.periodo_escolar_id=pe.id
          inner join grupo g on g.id=ag.id_grupo
-         inner join classe c on c.id=peca.id_classe
-where pe.id=2;
+         inner join classe c on c.id=peca.id_classe;
+
+ select * from ALUNOS_POR_GRUPO_VIEW i;
