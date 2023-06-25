8edbfa303bbcf59ac022c345d3310de202969e6f
Onesimo Martins <Ogouveia86@gmail.com>
2023-06-25 19:34:13 +0100

    feat: 3. lista das avaliações [incompleto]

--- querys.sql
+++ querys.sql
@@ -1,18 +1,9 @@
 select * from classe_disciplina;
 select * from classe;
 
--- seleccionar todas disciplinas de uma classe
-select c.numero as classe , d.nome as disciplina ,
- CONCAT(DATE_FORMAT(p.data_inicio,'%d/%m/%Y'),' a ', DATE_FORMAT(p.data_fim,'%d/%m/%Y') ) as  periodo_escolar
-
- from classe_disciplina  cd
-inner join disciplina d on d.id= cd.id_disciplina
-inner join classe c on c.id=cd.id_classe
-inner join periodo_escolar p on cd.periodo_escolar_id= p.id
-where p.id=1 and c.numero=1;
 
 
--- 1.	Lista dos alunos por grupo AINDA
+-- 1.	Lista dos alunos por grupo 
 select CONCAT(year(pe.data_inicio),'/',RIGHT((year(pe.data_fim)),2) ) as PERIODO_ESCOLAR,
        concat(c.numero,'a') as CLASSE,
        g.numero as GRUPO,
@@ -26,5 +17,17 @@ from aluno a
          inner join grupo g on g.id=ag.id_grupo
          inner join classe c on c.id=peca.id_classe
 where pe.id=2;
+
+-- 2.	Lista das disciplinas leccionadas em cada classe
+select c.numero as classe , d.nome as disciplina ,
+CONCAT(year(p.data_inicio),'/',RIGHT((year(p.data_fim)),2) ) as PERIODO_ESCOLAR 
+from classe_disciplina  cd
+inner join disciplina d on d.id= cd.id_disciplina
+inner join classe c on c.id=cd.id_classe
+inner join periodo_escolar p on cd.periodo_escolar_id= p.id
+where p.id=1 and c.numero=1;
+
+
+-- 3.	Lista dos alunos por grupo 
 
 # where 0=0 order by (pe.id);
