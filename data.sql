0b876ae4822b66020211c51a3fa06e685dc56628
Onesimo Martins <Ogouveia86@gmail.com>
2023-06-27 22:21:17 +0100

    chore: atualização dos dados

--- data.sql
+++ data.sql
@@ -4,60 +4,65 @@ use centro_estudantil;
 
 insert into classe(numero) values(1),(2),(3),(4),(5),(6);
 
-insert into aluno (nome,sobrenome,sexo) values 
-('Catia','Elias','F'),
-('Tomas','Machado','M'),
-('Antonio','Ngola','M'),
-('Patrick','Jack','M'),
-('Edson','Conceicao','M'),
-('Rafael','Miguel','M'),
-('Edna','Elias','F'),
-('Marilia','AG','F'),
-('Nadia','Mendonca','F'),
-('Ebora','Rafa','F'),
+insert into aluno (nome,sobrenome,sexo) values
+                                            ('Catia','Elias','F'),
+                                            ('Tomas','Machado','M'),
+                                            ('Antonio','Ngola','M'),
+                                            ('Patrick','Jack','M'),
+                                            ('Edson','Conceicao','M'),
+                                            ('Rafael','Miguel','M'),
+                                            ('Edna','Elias','F'),
+                                            ('Marilia','AG','F'),
+                                            ('Nadia','Mendonca','F'),
+                                            ('Ebora','Rafa','F'),
 
-('Edga','Felipe','F'),
-('Nadio','GG','M');
+                                            ('Edga','Felipe','F'),
+                                            ('Nadio','GG','M');
 
-insert into periodo_escolar(data_inicio,data_fim) values 
-('2020-01-09','2021-01-09'),
-('2021-01-09','2022-01-09'),
-('2022-01-09','2023-01-09'),
-('2023-01-09','2024-01-09');
+insert into periodo_escolar(data_inicio,data_fim) values
+                                                      ('2020-01-09','2021-01-09'),
+                                                      ('2021-01-09','2022-01-09'),
+                                                      ('2022-01-09','2023-01-09'),
+                                                      ('2023-01-09','2024-01-09');
+insert into periodo_escolar_classe_aluno(id_classe,id_aluno,periodo_escolar_id)
+values (1,1,1),(1,2,1),(1,3,1),(1,4,1),(1,11,1)  ,  (2,1,2),(2,2,2),(2,3,2),(1,4,2),(2,12,2);
 
-insert into grupo(numero) values (1),(2),(3), (1),(2),(3), (1),(2),(3), (1),(2),(3);
+insert into grupo(numero,classe_id) values (1,1),(2,1),(3,1),  (1,1),(2,1),(3,1);
 insert into aluno_grupo(id_aluno,id_grupo,periodo_escolar_id) values
- (1,1,1), (2,1,1),(3,1,1),
- (4,2,1),(5,2,1),(6,2,1),
- (7,3,1),(8,3,1),(9,3,1),(10,3,1),
- 
- (1,3,2), (2,3,2),(3,3,2),
- (4,2,2),(5,2,2),(6,2,2),
- (7,1,2),(8,1,2),(9,1,2),(10,1,2),
- 
- (1,1,3), (2,1,3),(3,1,3),
- (4,3,3),(5,3,3),(6,3,3),
- (7,2,3),(8,2,3),(9,2,3),(10,2,3),
- 
-(1,1,4),(2,1,4),(3,1,4),
-(4,2,4),(5,2,4),(6,2,4),
-(7,2,4),(8,2,4),(9,2,4),(10,2,4);
+                                                                  (1,1,1), (2,1,1),(3,1,1),
+                                                                  (4,2,1),(5,2,1),(6,2,1),
+                                                                  (7,3,1),(8,3,1),(9,3,1),(10,3,1),
+                                                                  (11,4,1),
 
-insert into tipo_baixa(descricao) values ('Reprovação'),('transferência'),('doença'),('culminação dos estudos');
+                                                                  (1,3,2), (2,3,2),(3,3,2),
+                                                                  (4,2,2),(5,2,2),(6,2,2),
+                                                                  (7,1,2),(8,1,2),(9,1,2),(10,1,2),
+                                                                  (12,3,2);
+
+#                                                                   (1,1,3), (2,1,3),(3,1,3),
+#                                                                   (4,3,3),(5,3,3),(6,3,3),
+#                                                                   (7,2,3),(8,2,3),(9,2,3),(10,2,3),
+#
+#                                                                   (1,1,4),(2,1,4),(3,1,4),
+#                                                                   (4,2,4),(5,2,4),(6,2,4),
+#                                                                   (7,2,4),(8,2,4),(9,2,4),(10,2,4);
+#
+
+                                                                  insert into tipo_baixa(descricao) values ('Reprovação'),('transferência'),('doença'),('culminação dos estudos');
 
 insert into aluno_baixa(id_aluno,id_baixa) values (11,2),(12,3);
 
 insert into disciplina(nome) values
-('Matemática'),('Língua portuguesa'),('Ed.Musical'),('Estudo do meio'),('Ed.Moral'),('Química'),('Física');
+                                 ('Matemática'),('Língua portuguesa'),('Ed.Musical'),('Estudo do meio'),('Ed.Moral'),('Química'),('Física');
 
-insert into classe_disciplina(id_classe,id_disciplina,periodo_escolar_id) values 
-(1,1,1),(2,1,1),(3,1,1),(4,1,1),(5,1,1),
-(1,2,1),(1,3,1),(2,3,1),(4,3,1),(6,7,1),(6,6,1);
+insert into classe_disciplina(id_classe,id_disciplina,periodo_escolar_id) values
+                                                                              (1,1,1),(2,1,1),(3,1,1),(4,1,1),(5,1,1),
+                                                                              (1,2,1),(1,3,1),(2,3,1),(4,3,1),(6,7,1),(6,6,1);
 
-insert into grupo(numero) values (1),(2),(3) , (1),(2),(3);
+insert into grupo(numero,classe_id) values (1,1),(2,1),(3,1) , (1,1),(2,1),(3,1);
 
-insert into avaliacao(id_disciplina,id_aluno,id_periodo_escolar,trimestre,nota) values 
-(1,1,1,1,8), (1,1,1,2,10), (1,1,1,3,2),
-(2,1,1,1,10), (2,1,1,2,10), (2,1,1,3,10),
-(3,1,1,1,3), (3,1,1,2,2), (3,1,1,3,0);
+insert into avaliacao(id_disciplina,id_aluno,id_periodo_escolar,trimestre,nota) values
+                                                                                    (1,1,1,1,8), (1,1,1,2,10), (1,1,1,3,2),
+                                                                                    (2,1,1,1,10), (2,1,1,2,10), (2,1,1,3,10),
+                                                                                    (3,1,1,1,3), (3,1,1,2,2), (3,1,1,3,0);
 
--- querys.sql
+++ querys.sql
@@ -65,9 +65,7 @@ select distinct CONCAT(year(pe.data_inicio),'/',RIGHT((year(pe.data_fim)),2) ) a
        ,g.numero as GRUPO
        ,al.id as CODIGO_ALUNO
        ,d.nome as DICIPLINA
-
 from avaliacao avaliacao
-
          inner join periodo_escolar_classe_aluno peca on peca.id_aluno = avaliacao.id_periodo_escolar
          inner join aluno al on al.id = avaliacao.id_aluno
          inner join periodo_escolar pe on pe.id = peca.periodo_escolar_id
@@ -75,5 +73,4 @@ from avaliacao avaliacao
          inner join aluno_grupo ag on ag.id_aluno=al.id and ag.periodo_escolar_id=pe.id
          inner join grupo g on g.id=ag.id_grupo
          inner join disciplina d on d.id=avaliacao.id_disciplina
-
 where   GET_DISCIPLINA_RESULT(d.id,al.id, pe.id) = 'REPROVADO' ;
